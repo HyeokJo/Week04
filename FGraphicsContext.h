3a5059d36e7d5c4ea06a8bedc9d7d548c8588649
이재용(JaeyongLEE) <roqkfwk7613@gmail.com>
2026-09-18 19:46:59 +0900

    chore: Delete Comment
    - 불필요한 주석 삭제

--- FGraphicsContext.h
+++ FGraphicsContext.h
@@ -25,55 +25,13 @@ public:
     FGraphicsContext(FGraphicsContext&&) = delete;
     FGraphicsContext& operator=(FGraphicsContext&&) = delete;
 
-    /*void Initialize(HWND WindowHandle, std::int32_t Width, std::int32_t Height);
-    void Shutdown();
-
-    void RequestResize(std::int32_t Width, std::int32_t Height);
-    bool ApplyPendingResize();
-
-    void BeginBackBufferRender();
-    void Present();*/
-
-
     void SetRenderer(FRenderer& InRenderer);
 
     FMessageChannel::FSender GetWindowToGraphicsSender();
 
     void Dispatch();
 
-    /*ID3D11Device* GetDevice() const
-    {
-        return Device.Get();
-    }
-
-    ID3D11DeviceContext* GetDeviceContext() const
-    {
-        return DeviceContext.Get();
-    }*/
-
 private:
-    /*void CreateDeviceAndSwapChain(
-        HWND WindowHandle,
-        std::int32_t Width,
-        std::int32_t Height);*/
-
-private:
-/*    struct FSize
-    {
-        std::int32_t Width = 0;
-        std::int32_t Height = 0;
-    };
-
-    Microsoft::WRL::ComPtr<ID3D11Device> Device;
-    Microsoft::WRL::ComPtr<ID3D11DeviceContext> DeviceContext;
-    Microsoft::WRL::ComPtr<IDXGISwapChain> SwapChain;
-
-    std::unique_ptr<IRenderSurface> BackBufferSurface;
-
-    FSize CurrentSize;
-    std::optional<FSize> PendingSize;
-
-    const float ClearColor[4] = { 0.2f, 0.2f, 0.7f, 1.0f };*/
 
     FRenderer* Renderer = nullptr; // 소유하지 않음
 
