26ae260a15c1ed445e525bbe75c2b10a433b6dc1
조혁 <56536360+HyeokJo@users.noreply.github.com>
2026-09-21 22:21:10 +0900

    [Fix] 바이너리 파일에서 Material Names 읽을 때 버그 수정

--- Serialize/FObjSerializer.cpp
+++ Serialize/FObjSerializer.cpp
@@ -41,7 +41,11 @@ bool FObjSerializer::SaveBinary(const FGeometry& GeometryData, const FString& Fi
 	//Material Names
 	uint32 MaterialNamesCount = static_cast<uint32>(GeometryData.MaterialNames.size());
 	Out.write(reinterpret_cast<const char*>(&MaterialNamesCount), sizeof(MaterialNamesCount));
-	Out.write(reinterpret_cast<const char*>(GeometryData.MaterialNames.data()), MaterialNamesCount * sizeof(FString));
+	for (int i = 0; i < MaterialNamesCount; i++)
+	{
+		WriteFString(Out, GeometryData.MaterialNames[i]);
+	}
+	//Out.write(reinterpret_cast<const char*>(GeometryData.MaterialNames.data()), MaterialNamesCount * sizeof(FString));
 
 	//SubMeshIndexCounts
 	uint32 SubMeshIndexCount = static_cast<uint32>(GeometryData.SubMeshIndexCounts.size());
@@ -98,7 +102,11 @@ bool FObjSerializer::LoadBinary(const FString& FilePath, FGeometry& OutGeoData)
 	uint32 MaterialNamesCount = 0;
 	In.read(reinterpret_cast<char*>(&MaterialNamesCount), sizeof(MaterialNamesCount));
 	OutGeoData.MaterialNames.resize(MaterialNamesCount);
-	In.read(reinterpret_cast<char*>(OutGeoData.MaterialNames.data()), sizeof(FString) * MaterialNamesCount);;
+	for (int i = 0; i < MaterialNamesCount; i++)
+	{
+		ReadFString(In, OutGeoData.MaterialNames[i]);
+	}
+	//In.read(reinterpret_cast<char*>(OutGeoData.MaterialNames.data()), sizeof(FString) * MaterialNamesCount);;
 
 	//SubMeshIndexCounts
 	uint32 SubMeshIndexCount = 0;
